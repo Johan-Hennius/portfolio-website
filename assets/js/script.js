c5703a9e239fa98532ad49f11b80c6bf610b385e
Johan Hennius <johan.bh19@gmail.com>
2023-11-03 08:36:44 +0000

    Add mobile nav menu - svg animation to be added

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,19 @@ elements.forEach((element) => {
         element.classList.remove("play");
     });
 });
+
+// Mobile navigation reveal animation
+const primaryNav = document.querySelector(".mobile-navigation__list");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+});
